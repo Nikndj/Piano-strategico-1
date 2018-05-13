87219d9d3803041922b9b52689f1bcf9c7f35c91
Nikndj <nicola.delgiudice@studenti.unicam.it>
2018-05-13 09:32:26 +0200

    Aggiunta Pagina ricerca utenti x admin

---
+++ AdminPage.php
@@ -0,0 +1,27 @@
+<?php 
+//parte la sessione
+session_start();
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Account</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <!--in questa pagina gli admin devono poter 
+    cercare gli utenti e poter cambiare il loro
+    livello di autorizzazione-->
+    <div class="container-fluid">
+    <form method="" action="">
+    <input type="text" name="usernameSearch" id="usernameSearch" placeholder="Utente da cercare">
+    <input type="submit" value="Cerca utente">
+    </form>
+    </div>
+
+</body>
+
+</html>
